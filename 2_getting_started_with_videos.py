b5bad3df3a3988de7a902159c0dde2d9de9051bc
dasaed <carlos.dasaed@gmail.com>
2019-05-26 10:47:49 -0230

    added comments to number 2

--- 2_getting_started_with_videos.py
+++ 2_getting_started_with_videos.py
@@ -2,51 +2,37 @@ import cv2
 
 print("Capturing video from your default camara")
 
-print("""\
-# Use the following command to capture an image from your default camara.
-however, it will only capture a single image, so you need to create a while
-loop to capture a video. The default camara is usually 0, or -1. If you have 
-more camaras, you will want to try values greater thatn 1 (1,2,3,...,etc)
+cap = cv2.VideoCapture(0) # Variable that will capture the camara frames
 
-cap = cv2.VideoCapture(0) # Variable that stores the video
-
-while(cap.isOpened()): #.isOpened() is a method that returns True or False, depending if the video source exists
-    ret, frame = cap.read() # ret returns false if the camara doesn't work
-                            # frame are the actual frames that you are capturing
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # converts video source into grayscale
-    cv2.imshow('First Video', gray) # Displays the video in gray scale
-    #cv2.imshow('First Video', frame) # Displays the video in normal mode
-    if cv2.waitKey(1) & 0xFF == ord('q'): # press 'q' to quit
-        break
-
-cap.release() # To release resources
-cv2.destroyAllWindows() # closes all windows
-""")
-
-cap = cv2.VideoCapture(0)
+#fourcc = determines the codec of the video, and there are 2 ways of using this:
 #fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-fourcc = cv2.VideoWriter_fourcc(*'XVID') #determines the codec for the video
-out = cv2.VideoWriter('/tmp/sampleVideo1.avi', fourcc, 20.0, (640,480))
-k = cv2.waitKey(0) & 0xFF
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-details = False
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
+#out = cv2.VideoWriter('output file name', codec, fps, Tuple(width,height)) #This is how we save the video to a file
+out = cv2.VideoWriter('./sampleVideo1.avi', fourcc, 20.0, (640,480))
+
+
+details = False # I used this variable so that we are not posting the details at every frame
+while(cap.isOpened()): # check if there is a video input. Returns false if the video input doesn't exist
+    ret, frame = cap.read() # ret = False if there isn't a frame detected. frame = the actual frame we are seeing
+    if ret == True: # if we do detect video input, show it and save it
         if details == False:
             print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             print(cap.get(cv2.CAP_PROP_FPS))
             details = True
-        out.write(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('First Video', gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        out.write(frame) # writes the input video to file
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # converts the input video to gray scale
+        cv2.imshow('First Video', gray) # shows the gray image. if we change gray to frame, it would
+        # display the normal image
+        #if cv2.waitKey(1) & 0xFF == 27: if we want to exit with the esc key
+        if cv2.waitKey(1) & 0xFF == ord('q'): # exits with the 'q' key
             break
     else:
         break
 
+# Now we need to release all the resources. Similar to how we should close a file after we are done working with it
 cap.release()
 out.release()
 cv2.destroyAllWindows()
